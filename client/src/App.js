8e9fd32a55b0ca128e184dae828d5f2322676abf
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-11-07 21:14:05 -0500

    created my update movies files now trying to get it to function properly

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import UpdateMovies from './Movies/UpdateMovies';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -15,12 +16,17 @@ const App = () => {
     <>
       <SavedList list={savedList} />
       <Route exact path="/" component={MovieList} />
+      <Route 
+        path='/update-movie/:id' 
+        render={props => <UpdateMovies {...props}/>}  />
       <Route
         path="/movies/:id"
         render={props => {
-          return <Movie {...props} addToSavedList={addToSavedList} />;
+          return <Movie {...props} addToSavedList={addToSavedList} />;     
         }}
       />
+       
+       
     </>
   );
 };
---
+++ client/src/Movies/UpdateMovies.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function UpdateMovies(props) {
+    const [movies, setMovies] = useState({
+        id: '',
+        title: '',
+        director: '',
+        metascore: 0,
+        stars: []
+    })
+
+    useEffect(() => {
+        axios
+            .get(`http://localhost:5000/api/movies/${props.match.params.id}`)
+            .then(res => {
+                setMovies(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+            
+    }, [props.match.params.id])
+
+    const handleChange = (e) => {
+        setMovies({
+            ...movies,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const handlechangestars = (e) => {
+        setMovies({
+            ...movies,
+            stars: [e.target.value]
+        })
+    }
+
+    const handleSubmit = (e, id) => {
+        e.preventDefault();
+        axios
+        .put(`http://localhost:5000/api/movies/${movies.id}`, movies)
+        .then(res => {
+            props.history.push('/')
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+    return (
+        <>
+        <h1>Update Movies</h1>
+        <form onSubmit={handleSubmit}>
+            <input 
+                type='text' 
+                name='title' 
+                placeholder='Title' 
+                value={movies.title} 
+                onChange={handleChange} />
+            <input 
+                type='text' 
+                name='director' 
+                placeholder='Director' 
+                value={movies.director} 
+                onChange={handleChange} />
+            <input 
+                type='number' 
+                name='metascore' 
+                placeholder='MetaScore' 
+                value={movies.metascore} 
+                onChange={handleChange} />
+            <input 
+                type='text' 
+                name='stars' 
+                placeholder='Actors' 
+                value={movies.stars} 
+                onChange={handlechangestars} />
+
+            <button type='submit'>Update</button> 
+        </form>
+
+        </>
+    )
+}
+
+export default UpdateMovies;
